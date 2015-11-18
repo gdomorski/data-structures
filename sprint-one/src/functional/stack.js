333e069c99e0424e81d547e9c47d857b9039ae14
gdomorski <greg.domorski@gmail.com>
2015-11-18 14:59:27 -0800

    functional stack completed

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,26 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    size++;
+    storage[size.toString()] = value;
   };
 
   someInstance.pop = function() {
+    var tempItem = storage[size.toString()];
+    delete storage[size.toString()];
+    size--;
+    return tempItem;
   };
 
   someInstance.size = function() {
+    if(size < 0) {
+      size = 0;
+    }
+    return size;
   };
 
   return someInstance;
