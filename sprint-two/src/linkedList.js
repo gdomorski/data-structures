fea790d32c4130fa7872032cc99c93a62b790447
gdomorski <greg.domorski@gmail.com>
2015-11-19 15:59:28 -0800

    completed linked list implementation

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,37 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if(this.head === null) {
+      this.head = Node(value);
+      this.tail = this.head;
+    } else {
+      var formerTail = this.tail;
+      this.tail = Node(value);
+      formerTail.next = this.tail;
+    }
   };
 
   list.removeHead = function() {
+    var formerHead = this.head;
+    this.head = Node(this.head).next;
+    if(this.head === null) this.head = this.tail;
+    return formerHead.value;
   };
 
   list.contains = function(target) {
+    var result = false;
+
+    var searchFn = function(obj) {
+      if(obj.value === target) result = true;
+      if(this.next === undefined)
+        return;
+      else
+        return searchFn(obj.next);
+    }
+
+    searchFn(this.tail);
+    searchFn(this.head);
+    return result;
   };
 
   return list;
