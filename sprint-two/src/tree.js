1858b7a35cd1c518757529758ee3c2d2028f83bd
gdomorski <greg.domorski@gmail.com>
2015-11-19 18:41:45 -0800

    2nd test passed

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,21 +1,25 @@
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  // your code here
-  newTree.children = null;  // fix me
+  this.children.push({value: value});
+  // console.log(this)
+
 };
 
 treeMethods.contains = function(target) {
+  _.each(this.children, function(item) {
+    console.log(item.value, target)
+    if(item.value === target) return true;
+  })
 };
 
 
