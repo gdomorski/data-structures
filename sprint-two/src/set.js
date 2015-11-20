c4b9d6aec068b1657a98a65714992730a923f16c
gdomorski <greg.domorski@gmail.com>
2015-11-20 11:38:36 -0800

    set fully implemented

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,16 +7,15 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  set._storage = item;
-  console.log(set._storage)
-  console.log(item);
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
- console.log('hey')
+ return !!this._storage[item];
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
